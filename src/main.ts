6fc9a14141987703a211bdc623449cb9a9c3b88c
yaoBohoussou <bohoussou1995@gmail.com>
2021-07-03 18:25:23 +0200

    F5 IT6 IT7 : connect to kafka & publish data to kafka topic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,59 @@
+import { Kafka } from "kafkajs";
 const csv = require('csv');
 const fs = require('fs');
 
-fs.createReadStream('./data/data.csv')
-	.pipe(csv.parse())
-	.pipe(csv.stringify())
-	.pipe(process.stdout)
 
+const kafka = new Kafka({
+  //clientId: "test-app",
+  brokers: ["localhost:9092"]
+});
+
+const producer = kafka.producer();/*kafka.producer({
+  maxInFlightRequests: 1,
+  idempotent: true,
+  transactionalId: "uniqueProducerId",
+});*/
+
+async function sendPayload(input: string) {
+  try {
+    await producer.send({
+      topic: "delivery_details",
+      messages: [{ /*key: "test",*/ value: input }],
+    });
+  } catch (e) {
+    console.error("Caught Error while sending:", e);
+  }
+}
+
+async function sendData(data : string)
+{
+	await producer.connect();
+	try
+	{
+	    await sendPayload(data);
+	    await producer.disconnect();
+	}
+	catch (e)
+	{
+	    console.error(e);
+	}
+}
+
+async function main() 
+{
+  await producer.connect();
+  try 
+  {
+      fs.createReadStream('./data/data.csv')
+      		.pipe(csv.parse())
+        	.pipe(csv.stringify())
+		.on('data', (data: string) => sendData(data.toString()));
+  } 
+  catch (e) 
+  {
+     console.error(e);
+  }
+  await producer.disconnect();
+}
+
+main();
